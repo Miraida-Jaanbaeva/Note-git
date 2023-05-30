991d7a99c0b0a48e19e5ccd1ee33b4083242845b
Mira <miraidajaanbaeva@gmail.com>
2023-05-30 19:01:24 +0600

    Add Note.kt

---
+++ domain/src/main/java/com/example/note_g_11/domain/models/Note.kt
@@ -0,0 +1,14 @@
+package com.example.note_g_11.domain.models
+
+import java.io.Serializable
+
+data class Note(
+    val id: Int = DEFAULT_ID,
+    val title: String,
+    val description: String,
+    val createdAt: Long
+) : Serializable {
+    companion object {
+        const val DEFAULT_ID = 0
+    }
+}
